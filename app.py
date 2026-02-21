a4d6a0cd8e87b82ecc1696c700ebad6b8936d0a6
Mohammed Batliwala <mattah43@gmail.com>
2026-02-21 22:53:10 +0000

    Add Flask Routes and html template to display top tracks

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World"
+    conn = sqlite3.connect("music.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT name, artist FROM tracks")
+    songs = cursor.fetchall()
+    conn.close
+    return render_template('index.html', songs = songs)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
